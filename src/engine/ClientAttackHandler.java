3559d8e8025337a43e7b9f382ed17a33d33055ba
= <evilsnowflake>
2017-06-26 20:11:46 -0400

    added print statements trying to fix the cant attack bug (reverted from commit e37d937d9df98872605483889b00c17ff0d91c12)

--- src/engine/ClientAttackHandler.java
+++ src/engine/ClientAttackHandler.java
@@ -22,10 +22,7 @@ public class ClientAttackHandler implements EventHandler<ActionEvent>{
 			GameCommand gc = new GameCommand("attack");
 			gc.displayMinion1 = fxd.attackingMinion;
 			gc.displayMinion2 = cb.minion;
-			
-			System.out.println(gc.displayMinion1.name + " attacking " + gc.displayMinion2.name);
 			fxd.attackingMinion = null;
-			
 			fxd.client.write(gc);
 		}
 		
